7aa38b3b668e35fd47a60e21dd075ba46f9b2d0c
Jose Jacsan <jjacsan@gmail.com>
2025-09-10 18:16:43 -0300

    Trabalhando com Listas

--- lib/pages/to_do_list_page.dart
+++ lib/pages/to_do_list_page.dart
@@ -37,10 +37,35 @@ class ToDoListPage extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(height: 8),
+              SizedBox(height: 16),
+
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  ListTile(
+                    title: Text('Tarefa 1'),
+                    subtitle: Text('10/08/2014'),
+                    leading: Icon(Icons.save),
+                    onTap: () {
+                      print('Tarefa 1');
+                    },
+                  ),
+                  ListTile(
+                    title: Text('Tarefa 2'),
+                    subtitle: Text('10/09/2025'),
+                    leading: Icon(Icons.person),
+                    onTap: () {
+                      print('Tarefa 2');
+                    },
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 16),
               Row(
                 children: [
                   Expanded(child: Text('Você possui 0 tarefas pendentes')),
+                  SizedBox(width: 8),
                   ElevatedButton(
                     onPressed: () {},
                     style: ElevatedButton.styleFrom(
